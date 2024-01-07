7040714786ce6697b14e2e1ca560e92fd58b0cf4
ygursu <yasemingrs@gmail.com>
2024-01-07 23:21:37 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,47 @@
+﻿public class Kitap
+{
+    public int sayfaSayisi;
+    public String kitapAdi, yazar;
+    Kitap(String kitapAdi, int sayfaSayisi, String yazar)
+    {
+        this.kitapAdi = kitapAdi;
+        this.sayfaSayisi = sayfaSayisi;
+        this.yazar = yazar;
+    }
+}
+public class Kitap
+{
+    public int sayfaSayisi;
+    public String kitapAdi, yazar;
+    Kitap(String kitapAdi, int sayfaSayisi, String yazar)
+    {
+        this.kitapAdi = kitapAdi;
+        this.yazar = yazar;
+        if (sayfaSayisi < 1)
+        {
+            this.sayfaSayisi = 10;
+        }
+        else
+        {
+            this.sayfaSayisi = sayfaSayisi;
+        }
+    }
+}
+public class Kitap
+{
+    private int sayfaSayisi;
+    private String kitapAdi, yazar;
+    Kitap(String kitapAdi, int sayfaSayisi, String yazar)
+    {
+        this.kitapAdi = kitapAdi;
+        this.yazar = yazar;
+        if (sayfaSayisi < 1)
+        {
+            this.sayfaSayisi = 10;
+        }
+        else
+        {
+            this.sayfaSayisi = sayfaSayisi;
+        }
+    }
+}
